b335161540c207ccf68c2cc407c75f3448fb8aa7
Oleksandr-Berd <alex.berd86@gmail.com>
2025-06-10 19:44:50 +0300

    add sharedLyout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import * as SC from "./AppStyled"
+import * as SC from "./AppStyled";
 
-import './App.css'
+import "./App.css";
+import SharedLayout from "./layout/SharedLayout/SharedLayout";
 
 function App() {
-
   return (
     <SC.AppStyled>
-     
+      <SharedLayout />
     </SC.AppStyled>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/layout/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,10 @@
+import * as SC from "./SharedLayoutStyled"
+const SharedLayout = () => {
+    return ( 
+        <SC.SharedLayoutStyled>
+
+        </SC.SharedLayoutStyled>
+     );
+}
+ 
+export default SharedLayout;
