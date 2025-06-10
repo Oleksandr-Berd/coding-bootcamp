ac73e244db1b2c5ed5c8eabb45fed0c995cb8d32
Oleksandr-Berd <alex.berd86@gmail.com>
2025-06-10 18:02:45 +0300

    launch

---
+++ src/componenst/TemplatePerson/TemplatePerson.jsx
@@ -0,0 +1,11 @@
+import * as SC from "./TemplatePersonStyled"
+
+const TemplatePerson = () => {
+    return ( 
+        <SC.TemplatePersonStyled>
+            
+        </SC.TemplatePersonStyled>
+     );
+}
+ 
+export default TemplatePerson;
